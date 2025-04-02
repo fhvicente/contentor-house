2b4cddd83d95ce39ac60ac5269ee4971dd595dff
Flavio <flaviohenriquevicente@hotmail.com>
2025-04-02 21:11:35 +0100

    favicon updated;

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ import Features from './components/features';
 import FeaturesSecond from './components/features-second';
 import FeaturesThird from './components/features-third';
 import ShelterList from './components/shelter';
-import Testimonials from './components/testimonials';
-
 
 const HomePage: React.FC = () => {
     return (
@@ -15,7 +13,6 @@ const HomePage: React.FC = () => {
             <ShelterList />
             <FeaturesSecond />
             <FeaturesThird />
-            {/* <Testimonials /> */}
         </>
     );
 };
